b7ef4df04dc3812ed24d00a58a82ccd4ca2170a3
GitHuberkata <petyagavrilova86@gmail.com>
2023-09-26 15:54:28 +0300

    Update secondmain.tf

--- workingdir/secondmain.tf
+++ workingdir/secondmain.tf
@@ -1,5 +1,6 @@
 #test with working directory
 #test with working directory and patterns radio button
+#test with working directory and added pattern "nlnkojmpod"
 
 
 resource "null_resource" "screen_output" {
