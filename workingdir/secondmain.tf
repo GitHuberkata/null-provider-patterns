ea353c95e69afb0d91abdb25bec98453fb214df4
GitHuberkata <petyagavrilova86@gmail.com>
2023-09-26 16:26:41 +0300

    Update secondmain.tf

--- workingdir/secondmain.tf
+++ workingdir/secondmain.tf
@@ -2,6 +2,7 @@
 #test with working directory and patterns radio button only
 #test with working directory and added new pattern line with content "nlnkojmpod"
 #test without entering working directory in the TFC Workspace and with radio button clicked
+#test without enterning working directory in the TFC workspace and with patterns radio button clicked + added empty line for new pattern
 
 
 resource "null_resource" "screen_output" {
